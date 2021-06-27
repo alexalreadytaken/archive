b74dc03c747d50365f3288981b044ffec6e2ba22
Егор <egornesvickij@gmail.com>
2021-06-27 10:47:12 +0500

    start of search development

--- frontend/src/components/search/searchFund.js
+++ frontend/src/components/search/searchFund.js
@@ -2,9 +2,10 @@ function search(){
   let inputFundNumber = document.getElementById('fundNumber');
   let inputFundName = document.getElementById('fundName');
 
-  console.log("hello")
-  console.log(inputFundNumber.value);
-  console.log(inputFundName.value);
+  fetch('http://10.3.0.87:3000/fund')
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(console.error('Что то не так...'));
 }
 
 
